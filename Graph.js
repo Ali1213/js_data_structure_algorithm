c8c6f9a3e3752f582b4b579fa23d6104e9f5cf41
Ali1213 <yusili2000@163.com>
2017-07-12 01:27:07 +0800

    feat:1.add graph,s 深度检索和广度检索

--- Graph.js
+++ Graph.js
@@ -28,6 +28,7 @@ class Graph {
     return s;
   }
 
+  //广度优先搜索
   bfs(v, cb) {
     let color = Graph.initColor(this[vertices]);
     let queue = new Queue();
@@ -46,6 +47,80 @@ class Graph {
       color[u] = 'black';
       cb && cb(u);
     }
+  }
+
+  // 寻找最短路径
+  BFS(v) {
+
+    let color = Graph.initColor(this[vertices]),
+      queue = new Queue(),
+      d = {},
+      pred = {};
+
+    queue.enqueue(v);
+
+    this[vertices].forEach(item => {
+      d[item] = 0;
+      pred[item] = null;
+    });
+
+
+    while (!queue.isEmpty()) {
+      let u = queue.dequeue(),
+        neighbors = this[adjList].get(u);
+
+      color[u] = 'grey';
+      neighbors.forEach((item) => {
+        if (color[item] == 'white') {
+          queue.enqueue(item);
+          color[item] = 'grey';
+
+          d[item] = d[u] + 1;
+          pred[item] = u;
+
+        }
+      });
+
+      color[u] = 'black';
+    }
+
+
+    return {
+      distances: d,
+      predecessors: pred
+    };
+  }
+
+  //深度优先搜索
+  dfs(cb) {
+    let color = Graph.initColor(this[vertices]),
+      _this = this;
+
+    const defVisit = function (u, color, cb) {
+
+      color[u] = 'grey';
+
+      cb && cb(u);
+
+
+      _this[adjList].get(u).forEach(item => {
+
+        if (color[item] === 'white') {
+          defVisit(item, color, cb);
+        }
+      });
+
+      color[u] = 'black';
+
+    };
+
+    this[vertices].forEach(item => {
+
+      if (color[item] === 'white') defVisit(item, color, cb);
+
+    });
+
+
   }
 
   static initColor(vertices) {
